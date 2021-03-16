3a4d4a948cb97d29865ccdacffb7176aae815f36
Talia-Apr <taliaaprianti46@gmail.com>
2021-03-16 22:08:44 +0700

    Tugas Minggu ke 11 SMT 4

---
+++ React JS vid 39-46/project-react-resto/react-resto/src/Back/Content.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import Kategori from "./Kategori";
+import Menu from "./Menu";
+import Pelanggan from "./Pelanggan";
+import Order from "./Order";
+
+const Content = () => {
+  const { isi } = useParams();
+
+  let tampil;
+
+  if (isi === "kategori") {
+    tampil = <Kategori />;
+  }
+
+  if (isi === "menu") {
+    tampil = <Menu />;
+  }
+
+  if (isi === "pelanggan") {
+    tampil = <Pelanggan />;
+  }
+
+  if (isi === "order") {
+    tampil = <Order />;
+  }
+
+  return <div>{tampil}</div>;
+};
+
+export default Content;
