5c4fc9925bab6289a39c53fa163c5e781143bbec
Talia-Apr <taliaaprianti46@gmail.com>
2021-03-31 13:51:45 +0700

    Tugas Minggu ke 13

---
+++ React JS vid 55-58/project-react-resto/react-resto/src/Back/Content.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import Kategori from "./Kategori";
+import Menu from "./Menu";
+import Pelanggan from "./Pelanggan";
+import Order from "./Order";
+import Detail from "./Detail";
+import User from "./User";
+
+const Content = () => {
+  const { isi } = useParams();
+
+  let tampil;
+
+  if (isi === "kategori") {
+    tampil = <Kategori />;
+  }
+
+  if (isi === "menu") {
+    tampil = <Menu />;
+  }
+
+  if (isi === "pelanggan") {
+    tampil = <Pelanggan />;
+  }
+
+  if (isi === "order") {
+    tampil = <Order />;
+  }
+
+  if (isi === "detail") {
+    tampil = <Detail />;
+  }
+
+  if (isi === "user") {
+    tampil = <User />;
+  }
+
+  return <div>{tampil}</div>;
+};
+
+export default Content;
---
+++ React JS vid 55-58/project-react-resto/react-resto/src/Back/Nav.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  const history = useHistory();
+
+  function hapus() {
+    sessionStorage.clear();
+    history.push("/login");
+  }
+
+  return (
+    <div className="mt-2 mb-2">
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <Link to="/admin">
+            <a className="navbar-brand">Admin Page</a>
+          </Link>
+
+          <li className="nav-item list-unstyled">
+            Email : {sessionStorage.getItem("email")}{" "}
+          </li>
+          <li className="nav-item list-unstyled">
+            Level : {sessionStorage.getItem("level")}
+          </li>
+          <button
+            onClick={hapus}
+            className="btn btn-outline-info"
+            type="submit"
+          >
+            Logout
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
